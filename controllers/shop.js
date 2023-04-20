1ffcd9eb84df41146e1100127ad2a6ce970ccee2
ashupn10 <ashupn10@gmail.com>
2023-04-20 14:14:44 +0530

    Added delete cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,78 +42,35 @@ exports.getIndex = async (req, res, next) => {
   }
 };
 
-exports.getCart = (req, res, next) => {
-  req.user
-    .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+exports.getCart =async (req, res, next) => {
+  const products=await req.user.getCart();
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart',
+    products: products
+  });
 };
 
 exports.postCart = async (req, res, next) => {
-  const prodId = req.body.productId;
-  const product=await Product.findById(prodId);
-  const result =await req.user.addToCart(product);
-  console.log(result);
-  res.redirect('/cart');
-
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findById(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //   })
-  //   .catch(err => console.log(err));
+  try{
+    const prodId = req.body.productId;
+    const product=await Product.findById(prodId);
+    const result =await req.user.addToCart(product);
+    res.redirect('/cart');
+  }catch(err){
+    console.log(err);
+  }
 };
 
-// exports.postCartDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   req.user
-//     .getCart()
-//     .then(cart => {
-//       return cart.getProducts({ where: { id: prodId } });
-//     })
-//     .then(products => {
-//       const product = products[0];
-//       return product.cartItem.destroy();
-//     })
-//     .then(result => {
-//       res.redirect('/cart');
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postCartDeleteProduct =async (req, res, next) => {
+  try{
+    const prodId = req.body.productId;
+    await req.user.removeItemCart(prodId);
+    res.redirect('/cart');
+  }catch(err){
+    console.log(err);
+  }
+};
 
 // exports.postOrder = (req, res, next) => {
 //   let fetchedCart;
--- models/user.js
+++ models/user.js
@@ -28,6 +28,37 @@ class User{
     return db.collection('users')
     .updateOne({_id:new mongodb.ObjectId(this._id)},{$set:{cart:updatedCart}});
   }
+  async getCart(){
+    const db=getDb();
+    const productIds=this.cart.items.map(i=>{
+      return i.productId;
+    })
+    const productArray=await db.collection('products').find({_id:{$in:productIds}}).toArray();
+    return productArray.map(p=>{
+      return {...p,quantity:this.cart.items.find(i=>{
+        return i.productId.toString()===p._id.toString();
+      }).quantity}
+    })
+  }
+  async removeItemCart(id){
+    const db=getDb();
+    const cartProductIndex=this.cart.items.findIndex(cp=>{
+      console.log('ID',id);
+      console.log('ProductId',cp.productId);
+      return cp.productId.toString()===id.toString();
+    })
+    let itemarray=this.cart.items;
+    if(cartProductIndex>=0){
+      if(itemarray[cartProductIndex].quantity>1){
+        itemarray[cartProductIndex].quantity--;
+      }else{
+        itemarray.splice(cartProductIndex,1);
+      }
+    }
+    return db.collection('users')
+    .updateOne({_id:new mongodb.ObjectId(this._id)},{$set:{cart:{items:itemarray}}});
+    
+  }
   static findUserById(id){
     const db=getDb();
     return db.collection('users').findOne({_id:new mongodb.ObjectId(id)});
